3eebb2df3d75d1671b7565771d6639958c7527bc
Alien Deployer <m@a17.name>
2022-01-21 01:50:37 +0300

    stake tokens ERC20Votes, ERC20Permit

--- contracts/XStakeBar.sol
+++ contracts/XStakeBar.sol
@@ -2,20 +2,23 @@
 
 pragma solidity 0.8.2;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 // X-Stake Bar is the coolest bar in town. You come in with some React, and leave with more! The longer you stay, the more React you get.
 //
 // This contract handles swapping to and from xReact, ReactSwap's staking token.
-contract XStakeBar is ERC20("X-Stake Bar", "xREACT") {
+contract XStakeBar is ERC20, ERC20Permit, ERC20Votes {
     using SafeMath for uint256;
 
     IERC20 public react;
 
     // Define the React token contract
-    constructor(IERC20 _react) {
+    constructor(
+        IERC20 _react
+    ) ERC20("X-Stake Bar", "xREACT") ERC20Permit("X-Stake Bar")  {
         react = _react;
     }
 
@@ -48,5 +51,28 @@ contract XStakeBar is ERC20("X-Stake Bar", "xREACT") {
         uint256 what = _share.mul(react.balanceOf(address(this))).div(totalShares);
         _burn(msg.sender, _share);
         react.transfer(msg.sender, what);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+    internal
+    override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+    internal
+    override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+    internal
+    override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
     }
 }
